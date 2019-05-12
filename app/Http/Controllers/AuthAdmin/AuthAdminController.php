123f28cecbbf1bfb6c5526cad3d9a71f95408a9d
royhan31 <izzapark34@gmail.com>
2019-05-12 23:48:18 +0700

    Admin & Company

---
+++ app/Http/Controllers/AuthAdmin/AuthAdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\AuthAdmin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthAdminController extends Controller
+{
+    //
+}
