efc728eedc681a572c1a29e6973e48c03b802fc8
royhan31 <izzapark34@gmail.com>
2019-05-15 16:39:05 +0700

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => '/'], function(){
   Route::get('login','AuthCompany\AuthCompanyController@showLoginForm')->name('login');
-  Route::post('login', 'AuthCompany\AuthCompanyController@login')->name('company.login');
   Route::get('register','AuthCompany\AuthCompanyController@showRegisterForm')->name('register');
-  Route::post('postregister', 'AuthCompany\AuthCompanyController@register')->name('company.register');
-  Route::get('company/dashboard', 'AuthCompany\AuthCompanyController@index')->name('company.dashboard');
-
   Route::post('login','AuthCompany\AuthCompanyController@login')->name('get.login');
-  Route::get('register','AuthCompany\AuthCompanyController@showRegisterForm')->name('register');
   Route::post('register','AuthCompany\AuthCompanyController@register')->name('get.register');
   Route::post('logout','AuthCompany\AuthCompanyController@logoutCompany')->name('logout');
   Route::get('dashboard','Company\CompanyController@index')->name('dashboard');
@@ -43,4 +38,3 @@ Route::get('/admin', 'AuthAdmin\AuthAdminController@index')->name('admin');
 //Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
-
