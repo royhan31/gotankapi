e47b3afe1dd3b5db71c73f981f74886a3cac9ea4
Nurusshofa95 <gotank2019@gmail.com>
2019-05-16 17:22:27 +0700

    Register company ok

--- app/Http/Controllers/AuthCompany/AuthCompanyController.php
+++ app/Http/Controllers/AuthCompany/AuthCompanyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Validation\Validator;
 use Auth;
+use App\Company;
 
 class AuthCompanyController extends Controller{
 
